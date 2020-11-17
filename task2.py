0e547ff15f94f4b4becb8f3833a0bf2ae236c2e4
cyler88 <67733858+cyler88@users.noreply.github.com>
2020-11-17 15:05:21 +0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,10 @@
 def odometer(N):
-    d = 0
-    for i in range(0, len(N), 2):
-        d += N[i] 
-    return d
+    chet = [i for i in range(len(N)) if not i % 2]
+    nechet = [i for i in range(len(N)) if i % 2]
+    D = 0
+    for i in range(len(chet)):
+        if i == 0:
+            D += N[chet[i]] * N[nechet[i]]
+        else:
+            D += (N[chet[i]] * (N[nechet[i]] - N[nechet[i - 1]]))
+    return D
